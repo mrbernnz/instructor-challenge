9ba50caf62ade70188b4c9abac6dc5517b9ebdaf
mrbernnz <noel.sagaille@gmail.com>
2018-03-01 10:29:15 -0500

    * Need to bind PORT to listener.
      Why:
    
        Heroku error logs persisted with this error.

--- server.js
+++ server.js
@@ -34,7 +34,9 @@ app.post('/favorites', function(req, res) {
   res.send(data);
 });
 
+app.set('port', process.env.PORT || 3000);
+
 // Syntax error. list !== listen.
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Listening on port', app.get('port'));
 });
